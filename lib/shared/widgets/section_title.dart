07790fbac6882dcda1423590ad743b6161409180
JGabrielGlez <jegagonzalezes@ittepic.edu.mx>
2026-03-01 17:15:36 -0700

    SectionTitle creado

--- lib/shared/widgets/section_title.dart
+++ lib/shared/widgets/section_title.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import '../../core/theme/app_colors.dart';
+import '../../core/theme/app_text_styles.dart';
 
 class SectionTitle extends StatelessWidget {
-  const SectionTitle({super.key});
+  // Lo que recibe desde fuera es el texto a mostrar
+  final String titulo;
+
+  // Constructor, como siempre se renderiza, ocupa tener un id, que es la key, posteriormente, siempre pedir lo que es el titulo a mostrar
+  const SectionTitle({super.key, required this.titulo});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text(titulo, style: AppTextStyles.tituloPantalla);
   }
 }
