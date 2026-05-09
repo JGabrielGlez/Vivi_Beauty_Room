47e0340a78725f9f51b9378c8458d49f2b6b7bf4
Rocio <rovazquezla@ittepic.edu.mx>
2026-05-08 19:15:25 -0700

    feat: activar ruta servicios en index

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use('/api/auth', authRoutes);
 // Rutas protegidas (con auth)
 // app.use('/api/agenda',    authMiddleware, require('./src/routes/agenda'));
 // app.use('/api/citas',     authMiddleware, require('./src/routes/citas'));
-// app.use('/api/servicios', authMiddleware, require('./src/routes/servicios'));
+ app.use('/api/servicios', authMiddleware, require('./src/routes/servicios'));
 // app.use('/api/clientas',  authMiddleware, require('./src/routes/clientas'));
 
 const PORT = process.env.PORT || 3000;
