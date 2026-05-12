3c83bb00c2c159a4a28239cdcc9a1fe50171d225
Miguel <minunezpe@ittepic.edu.mx>
2026-05-12 01:19:25 -0700

    Pirmera prueba de ejecutable

--- backend/src/db/schema.js
+++ backend/src/db/schema.js
@@ -1,6 +1,7 @@
 // src/db/schema.js
 const Database = require("better-sqlite3");
-const db = new Database("database.db");
+const dbPath = process.env.SQLITE_PATH || "database.db";
+const db = new Database(dbPath);
 
 // Usuarios
 db.prepare(
